4a4763ca7097297c2edc3e7f1d422a1d4e4d56b7
B4ST1N2 <svanegas899@soyudemedellin.edu.co>
2024-09-26 11:05:23 +0000

    prueba

--- lib/src/pages/HomePage/HomePage.dart
+++ lib/src/pages/HomePage/HomePage.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/material.dart'; 
-import 'package:myapp/src/pages/HomePage/tabs/movies.dart'; 
-import 'package:myapp/src/pages/HomePage/tabs/series.dart'; 
-import 'package:myapp/src/pages/HomePage/tabs/top.dart'; 
+import 'package:flutter/material.dart';
+import 'package:myapp/src/pages/HomePage/tabs/movies.dart';
+import 'package:myapp/src/pages/HomePage/tabs/series.dart';
+import 'package:myapp/src/pages/HomePage/tabs/top.dart';
 
 class homePage extends StatelessWidget {
   const homePage({
@@ -12,18 +12,26 @@ class homePage extends StatelessWidget {
   Widget build(BuildContext context) {
     // Widget padre que controla las pestañas (tabs).
     return DefaultTabController(
-      length: 3, 
+      length: 3,
       child: Scaffold(
         appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 228, 211, 64), // Define el color de fondo del AppBar.
-          title: const Text('IMBXD'), // Título centrado que aparece en el AppBar.
+          backgroundColor: Color.fromARGB(
+              255, 228, 211, 64), // Define el color de fondo del AppBar.
+          title:
+              const Text('IMBXD'), // Título centrado que aparece en el AppBar.
           centerTitle: true, // Alinea el título en el centro del AppBar.
           bottom: const TabBar(
             // Crea la barra de pestañas en la parte inferior del AppBar.
             tabs: [
-              Tab(icon: Icon(Icons.movie)), // Primera pestaña con un icono de película.
-              Tab(icon: Icon(Icons.tv)), // Segunda pestaña con un icono de televisión.
-              Tab(icon: Icon(Icons.topic)), // Tercera pestaña con un icono de tema.
+              Tab(
+                  icon: Icon(Icons
+                      .movie)), // Primera pestaña con un icono de película.
+              Tab(
+                  icon: Icon(
+                      Icons.tv)), // Segunda pestaña con un icono de televisión.
+              Tab(
+                  icon: Icon(
+                      Icons.topic)), // Tercera pestaña con un icono de tema.
             ],
           ),
         ),
